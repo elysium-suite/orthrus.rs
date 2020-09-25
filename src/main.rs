28258ef8503a775d8de9904aed9a395735f01401
safinsingh <safin.singh@gmail.com>
2020-09-25 13:26:20 -0700

    add proper error handling for incorrect password

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 use dotenv::dotenv;
-use rocket::response::NamedFile;
+use rocket::http;
+use rocket::request::Request;
+use rocket::response::{self, NamedFile, Responder, Response};
+use serde::Serialize;
 
 use std::env;
 use std::io;
@@ -9,12 +12,41 @@ use std::path::Path;
 #[macro_use]
 extern crate rocket;
 
+#[derive(Debug, Serialize)]
+struct PassErr {
+    message: String,
+    tried_pass: String,
+}
+
+impl PassErr {
+    fn new(m: &str, t: &str) -> PassErr {
+        PassErr {
+            message: m.into(),
+            tried_pass: t.into(),
+        }
+    }
+}
+
+impl<'r> Responder<'r> for Error {
+    fn respond_to(self, _: &Request) -> response::Result<'r> {
+        if let Error::Pass(pass_err) = self {
+            let res = PassErr::new(&pass_err.message, &pass_err.tried_pass);
+            return Response::build()
+                .sized_body(io::Cursor::new(serde_json::to_string(&res).unwrap()))
+                .raw_header("Content-Type", "application/json")
+                .ok();
+        }
+        Err(http::Status::new(404, "Not Found"))
+    }
+}
+
 // Create custom error enum to
 // wrap possible returns
 #[derive(Debug)]
 enum Error {
     Env(std::env::VarError),
-    Io(std::io::Error),
+    Io(io::Error),
+    Pass(PassErr),
 }
 
 // Make sure directory structure is compliant
@@ -40,10 +72,7 @@ fn index(os: String, pass: String) -> Result<NamedFile, Error> {
     if pass == key {
         NamedFile::open(Path::new("dist").join(format!("aeacus-{}.zip", os))).map_err(Error::Io)
     } else {
-        Err(Error::Io(io::Error::new(
-            io::ErrorKind::PermissionDenied,
-            "Wrong pass!",
-        )))
+        Err(PassErr::new("Wrong password!", &pass)).map_err(Error::Pass)
     }
 }
 
