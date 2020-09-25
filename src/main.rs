911b2ef016fe7e9d7c11774106fd445efcff04d3
safinsingh <safin.singh@gmail.com>
2020-09-25 12:35:12 -0700

    refactor everything into actually readable and idiomatic code because calc is really boring

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,61 @@
 #![feature(proc_macro_hygiene, decl_macro)]
+use dotenv::dotenv;
 use rocket::response::NamedFile;
-use std::fs;
-use std::io::Error;
-use std::io::ErrorKind;
+
+use std::env;
+use std::io;
 use std::path::Path;
 
 #[macro_use]
 extern crate rocket;
 
-fn return_file(file_name: String, pass: Option<String>) -> Result<NamedFile, Error> {
-    let auth = fs::read_to_string("pass.txt");
-    let auth = match auth {
-        Ok(o) => {
-            if o.is_empty() {
-                "default_secure_password".to_string()
-            } else {
-                o
-            }
-        }
-        Err(_) => "default_secure_password".to_string(),
-    };
-
-    match pass {
-        Some(p) => {
-            if p == auth {
-                let f =
-                    NamedFile::open(Path::new("static/").join(format!("aeacus-{}.zip", file_name)));
-                match f {
-                    Ok(f) => Ok(f),
-                    Err(err) => Err(err),
-                }
-            } else {
-                Err(Error::new(ErrorKind::PermissionDenied, "Wrong pass!"))
-            }
-        }
-        None => Err(Error::new(ErrorKind::PermissionDenied, "No pass!")),
-    }
+// Create custom error enum to
+// wrap possible returns
+#[derive(Debug)]
+enum Error {
+    Env(std::env::VarError),
+    Io(std::io::Error),
 }
 
+// Make sure directory structure is compliant
+fn check() -> bool {
+    if Path::new(".env").exists()
+        && Path::new("dist/aeacus-win32.zip").exists()
+        && Path::new("dist/aeacus-linux.zip").exists()
+    {
+        return true;
+    }
+    false
+}
+
+// Main route for server
 #[get("/?<os>&<pass>")]
-fn index(os: Option<String>, pass: Option<String>) -> Result<NamedFile, String> {
-    match os {
-        Some(o) => match o.as_str() {
-            "win32" => {
-                let res = return_file("win32".to_string(), pass);
-                match res {
-                    Ok(r) => Ok(r),
-                    Err(e) => Err(format!("{}", e)),
-                }
-            }
-            "linux" => {
-                let res = return_file("linux".to_string(), pass);
-                match res {
-                    Ok(r) => Ok(r),
-                    Err(e) => Err(format!("{}", e)),
-                }
-            }
-            _ => Err(format!("Invalid OS!")),
-        },
-        None => Err(format!("No OS!")),
+fn index(os: String, pass: String) -> Result<NamedFile, Error> {
+    // Read KEY environment variable into string
+    // and fail if it's not present
+    let key = env::var("KEY").map_err(Error::Env)?;
+
+    // If the password equals the key, return
+    // the appropriate file
+    if pass == key {
+        NamedFile::open(Path::new("dist").join(format!("aeacus-{}.zip", os))).map_err(Error::Io)
+    } else {
+        Err(Error::Io(io::Error::new(
+            io::ErrorKind::PermissionDenied,
+            "Wrong pass!",
+        )))
     }
 }
 
 fn main() {
+    // Load enviroment variables
+    dotenv().ok();
+
+    // Run path structure check
+    if !check() {
+        panic!("Required files do not exist!");
+    }
+
+    // Initialize web server
     rocket::ignite().mount("/", routes![index]).launch();
 }
